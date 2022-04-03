238688d61ce3ee7e860973d12d8640bcf099cba3
William Luke <williamluke4@gmail.com>
2022-04-03 07:52:23 +0300

    add linux icon support

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -46,7 +46,7 @@ static void my_application_activate(GApplication* application) {
   } else {
     gtk_window_set_title(window, "mySarafu");
   }
-
+  gtk_window_set_icon_from_file(window, "assets/images/sarafu_logo.png", NULL);
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
 
